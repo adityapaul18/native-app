678fe4a7a6ba4002a85a1364d0cd1e97f2fc45fa
Aditya Paul <aditya.paul18@gmail.com>
2021-06-25 14:14:17 +0530

    start

---
+++ Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBZeFmeGW7NmtCRSSgarI-bISmu3s_Liik",
+    authDomain: "signal-4ae86.firebaseapp.com",
+    projectId: "signal-4ae86",
+    storageBucket: "signal-4ae86.appspot.com",
+    messagingSenderId: "501505903992",
+    appId: "1:501505903992:web:16ef32deaa5e0a86fe4c8d",
+    measurementId: "G-8WE19SMRC8"
+  };
