6185edbfe542a45eda233b4fc4bb8d30c8501fde
Aditya Paul <aditya.paul18@gmail.com>
2021-12-08 17:00:44 +0530

    add chat screen

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Loginscreen from "./screens/Loginscreen";
 import { createStackNavigator } from "@react-navigation/stack";
 import RegisterScreen from "./screens/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
+import AddChatScreen from "./screens/AddChatScreen";
 
 const Stack = createStackNavigator();
 const globalScreenOptions = {
@@ -21,8 +22,8 @@ export default function App() {
             <Stack.Screen name="Login" component={Loginscreen} />
             <Stack.Screen name="Register" component={RegisterScreen} />
 			 <Stack.Screen name="Home" component={HomeScreen} />
-			{/*<Stack.Screen name="AddChat" component={AddChatScreen} />
-			<Stack.Screen name="Chat" component={ChatScreen} /> */}
+			<Stack.Screen name="AddChat" component={AddChatScreen} />
+			{/* <Stack.Screen name="Chat" component={ChatScreen} /> */}
         </Stack.Navigator>
     </NavigationContainer>
   );
